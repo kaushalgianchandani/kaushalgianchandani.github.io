14e6997cf167da58688fc045aace3bec2f1297f8
Kaushal Gianchandani <kaushal.gcd@gmail.com>
2026-01-29 13:02:48 -0500

    update Nathan Paldor hyperlink

--- data/teaching.js
+++ data/teaching.js
@@ -40,7 +40,7 @@ const teaching = [
         university: "Hebrew University of Jerusalem",
         term: "Fall 2020",
         description: `
-        <p> I served as the teaching assistant for a graduate-level course on mathematical methods in scientific modeling, taught by Prof. <a href="https://en.earth.huji.ac.il/people/nathan-paldor">Nathan Paldor</a>. I worked with students during problem-solving sessions, helping them navigate unfamiliar mathematical tools and address gaps in quantitative preparation. I also developed and expanded the lecture notes with step-by-step derivations and intuitive explanations to make key methods easier to follow and apply. The notes are available below.</p>
+        <p> I served as the teaching assistant for a graduate-level course on mathematical methods in scientific modeling, taught by Prof. <a href="https://en.earth.huji.ac.il/people/nathan-paldor" target="_blank" rel="noopener noreferrer">Nathan Paldor</a>. I worked with students during problem-solving sessions, helping them navigate unfamiliar mathematical tools and address gaps in quantitative preparation. I also developed and expanded the lecture notes with step-by-step derivations and intuitive explanations to make key methods easier to follow and apply. The notes are available below.</p>
         `,
         resources: [
             { label: "Lecture Notes", url: "pdfs/lecture_notes_mmism.pdf", icon: "far fa-file-pdf" }
